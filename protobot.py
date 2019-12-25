67e70ae37598078b52f0bf7674f84f022a03e0e0
Konnor Klercke <klercke@protonmail.com>
2019-12-25 16:38:45 -0500

    initial commit

---
+++ protobot.py
@@ -0,0 +1,26 @@
+"""
+Author: Konnor Klercke
+File: protobot.py
+Purpose: Discord bot for my personal server
+"""
+
+
+#################################
+                                #
+import os                       #
+import discord                  #
+from dotenv import load_dotenv  #
+                                #
+#################################
+
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(token)

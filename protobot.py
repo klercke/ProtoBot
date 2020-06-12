ca265c8277c338ec5af589949e708640ad773243
Konnor Klercke <klercke@protonmail.com>
2020-06-12 11:00:00 -0400

    Remove TWSS, B99, and raise-exception. Update event handlers (on_connect, on_ready, on_disconnect).

--- protobot.py
+++ protobot.py
@@ -15,28 +15,56 @@ from discord.ext import commands    #
                                     #
 #####################################
 
-#########################################
-                                        #
-bot = commands.Bot(command_prefix='!')  #
-VERSION = "v0.1-alpha"                  #
-ACTIVITY = discord.Game("!help")        #
-                                        #
-#########################################
+#####################################
+                                    #
+COMMAND_PREFIX = '!'                #
+VERSION = "v0.1-alpha"              #
+ACTIVITY = discord.Game("!help")    #
+                                    #
+#####################################
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')  
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix=COMMAND_PREFIX)  
+
+
+@bot.event
+async def on_connect():
+    """
+    Prints a message with bot name and version when bot connects to Discord servers.
+    Sets the bot activity to ACTIVITY.
+    """
+
+    print(f'{bot.user.name} {VERSION} has sucessfully connected to Discord.')
+    await bot.change_presence(activity = ACTIVITY)
 
 
 @bot.event 
 async def on_ready():
     """
-    Logs a message with bot name and version when the bot starts. Sets the
-    bot activity to ACTIVITY.
+    Prints a list of guilds the bot is connected to when the bot is finished processing
+    date from Discord servers.
     """
 
-    print(f'{bot.user.name} {VERSION} has sucessfully connected to Discord.')
-    await bot.change_presence(activity = ACTIVITY)
+    print('Bot loading complete. Current guilds: ', end='')
     
+    guilds = []
+    for guild in bot.guilds:
+        label = guild.name + " (" + str(guild.id) + ")"
+        guilds.append(label)
+
+    print(*guilds, sep=', ')
+
+
+@bot.event
+async def on_disconnect():
+    """
+    Prints a message when bot disconnects from Discord. Usually this is temporary.
+    """
+
+    print('Lost connection to Discord.')
+
 
 
 @bot.event
@@ -116,54 +144,5 @@ async def on_message(message):
                     await message.channel.send(response)
                     break
     
-    if 'hard' in message.content.lower() or 'long' in message.content.lower() or 'wet' in message.content.lower() or 'suck' in message.content.lower():
-        """
-        Lets the bot tell the famous "That's what she said" joke
-        """
-
-        user_message = message.content.split()
-        
-        dirty_words = [' hard', ' long', ' wet', ' suck']
-
-        for i in range(len(user_message)):
-            word = user_message[i].lower().strip("?").strip('"').strip("'")
-
-            if (' ' + word in dirty_words):
-
-                print("That's what she said incoming!")
-    
-                response = "That's what she said!"
-                print(response)
-                await message.channel.send(response)
-                break
-
-
-@bot.command(name = '99', help = 'Responds with a random Brooklyn 99 quote.')
-async def nine_nine(ctx):
-    b99_quotes = [
-        "I'm the human form of the 💯 emoji.",
-        "Bingpot!",
-        "Nine-nine!",
-        (
-            "Cool. Cool cool cool cool cool cool cool, "
-            "no doubt no doubt no doubt no doubt."
-        ),
-        "With all due respect, I am gonna completely ignore everything you just said.",
-        (
-            "The English language can not fully capture the depth and complexity of my "
-            "thoughts, so I’m incorporating emojis into my speech to better express myself. 😉."
-        ),
-        "I’d like your $8-est bottle of wine, please.",
-        "But if you’re here, who’s guarding Hades?"
-    ]
-
-    response = random.choice(b99_quotes)
-    await ctx.send(response)
-
-
-@bot.command(name = 'raise-exception', help = 'Logs an exception with message data. Developer use.')
-async def raise_exception(ctx):
-    await ctx.send('Raising an exception to my console...')
-    raise discord.DiscordException
 
 bot.run(TOKEN)

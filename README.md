0b10cdb45261a96dc6aa00ebf9672fcea7d175dc
Konnor Klercke <klercke@protonmail.com>
2024-01-05 18:07:18 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,22 +1,4 @@
-# ProtoBot
-A do-it-all Discord bot
+# ProtoBotRS
+This branch is for the upcoming ProtoBot relaunch (written in Rust!)
 
 This bot is a work in progress. It was originally made for my growing discord server, but is being expanded to be a general-purpose, modular bot. Any community contributions are welcome.
-
-## Install
-First install all the python modules needed:
-```
-sudo python3 -m pip install -U py-cord
-sudo python3 -m pip install -U python-dotenv
-sudo python3 -m pip install -U schedule
-sudo python3 -m pip install -U sympy
-```
-
-After installing of of the modules, you will need to make a file in the ProtoBot folder called ".env" and paste the following into it:
-```
-# .env
-DISCORD_TOKEN=[TOKEN]
-```
-where [TOKEN] is replaced with a [Discord Bot Token](https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token)
-
-Note: To use the !tex command, you will need to make sure LaTeX is installed on the server running the bot (usually called TeXLive-full on Linux)
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::env;
+
+use dotenv;
+
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, Configuration, CommandResult};
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new().group(&GENERAL_GROUP);
+    // Set command prefix to "!"
+    framework.configure(Configuration::new().prefix("!"));
+
+    // Load variables from .env file
+    dotenv::dotenv().ok();
+
+    // Log in with bot token from .env
+    let token = env::var("DISCORD_TOKEN").expect("Error reading bot token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await.expect("Error creating client");
+
+    // Create a single shard and start listening for events
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}

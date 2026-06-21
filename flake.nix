c5f517da3ed96ce9aeab85b43b57c76014c3b61b
Konnor Klercke <klercke@protonmail.com>
2026-06-21 12:37:18 -0400

    ci: add nix

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  inputs = {
+    rust-overlay.url = "github:oxalica/rust-overlay/stable";
+    cargo2nix =  {
+      url = "github:cargo2nix/cargo2nix/release-0.12";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  # Pass through all inputs and bring them into scope
+  outputs = inputs: with inputs;
+
+    # Build the output set for each default system and map system sets into
+    # attributes, resulting in paths such as:
+    # nix build .#packages.x86_64-linux.<name>
+    flake-utils.lib.eachDefaultSystem ( system:
+      let
+        
+        # Create nixpkgs containing rustBuilder from cargo2nix overlay
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            cargo2nix.overlays.default
+            rust-overlay.overlays.default
+          ];
+
+        };
+        
+        rustVersion = "1.93.0";
+
+        # Create workspace and dependencies package set
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          inherit rustVersion;
+          packageFun = import ./Cargo.nix;
+        };
+
+        # Create workspace/dev shell
+        workspaceShell = (rustPkgs.workspaceShell {
+          packages = with pkgs; [
+            rust-bin.stable.${rustVersion}.default
+            rust-analyzer
+            clippy
+            git-cliff
+          ];
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        });
+
+
+      # Recursive output set for each system
+      in rec {
+        devShells = {
+          default = workspaceShell;
+        };
+
+        # Package build
+        packages = {
+          # nix build .#protochess
+          # nix build .#packages.x86_64-linux.protochess
+          protochess = (rustPkgs.workspace.protochess {});
+          # nix build
+          default = packages.protochess;
+        };
+      }
+    );
+}
